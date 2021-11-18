5973ace595392b25a479131489ecd4f1aae2962e
Splat789 <mcwulfson@gmail.com>
2021-11-18 09:11:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-hivh2v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-hivh2v)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Ha Ha, I Tricked You !</h1>`;
+window.alert(5 + 6);
