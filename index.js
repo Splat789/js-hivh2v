496b401ddb27087f6f1713e58db937357f611ca0
Splat789 <94552482+Splat789@users.noreply.github.com>
2023-04-27 14:40:17 -0500

    1 file modified

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Ha Ha, AppSec (Matt) Tricked You !</h1>`;
-window.alert(5 + 6);
+window.alert('XSS Code Runs in Here');
