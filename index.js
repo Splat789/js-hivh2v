08462fae4f33f58d3daf50d45d94717c04ab2da7
Splat789 <94552482+Splat789@users.noreply.github.com>
2023-04-27 14:37:32 -0500

    1 file modified

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Ha Ha, I Tricked You !</h1>`;
+appDiv.innerHTML = `<h1>Ha Ha, AppSec (Matt) Tricked You !</h1>`;
 window.alert(5 + 6);
